a038f32ca07a166be6a7c12cd24dc879a69ef301
Marcelo Rodrigues <85708828+marcelo-mmdev@users.noreply.github.com>
2023-08-09 17:44:44 -0300

    Instalando chakra e estrutura da page login

--- src/pages/homePage/index.tsx
+++ src/pages/homePage/index.tsx
@@ -1,10 +1,42 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import React from "react";
 
 const HomePage = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
-      <Box>HomePage</Box>
+      <Button onClick={onOpen}>Detalhes</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {/* <Lorem count={2} /> */} kfndlsakjfhnlfhsd sfhvsndlfbsv
+            hfsdnhfvlcnsdfvs vsdflvusdnfvu sdfv lsdkfnhv sdf,mgjnvsdlkfvuns
+            dlgbsd
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 };
