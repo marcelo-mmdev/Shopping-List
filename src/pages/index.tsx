26897ad1e37ada1e8f730a75ebb2cb645e005812
Marcelo Rodrigues <85708828+marcelo-mmdev@users.noreply.github.com>
2023-08-09 15:48:45 -0300

    Project sem nada com tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
+import Image from "next/image";
 import { Inter } from "next/font/google";
-// import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <div>Olá Mundo</div>
+      <div>Olá Mundo!!!</div>
     </>
   );
 }
